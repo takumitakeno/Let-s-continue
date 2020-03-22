dc4fabcb6315b8a0c39c61a9e1040c18b7380804
takumitakeno <matchball.shine@gmail.com>
2020-03-22 07:47:49 +0000

    ユーザー登録、ログイン、ログアウト機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,13 @@ Rails.application.routes.draw do
   # scope :admins do
   # 	resources :users
   # end
-
+  root "homes#about"
   get "homes/about" => "homes#about"
   get "users/bmi" => "homes#bmi"
   resources :users
 
   namespace :admin do
   	# get "/users" => "admin_users#index"
+  	resources :users
   end
-  # root "users/homes#about"
 end
