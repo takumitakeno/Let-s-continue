2818d25b7dbf35c21ae2871540527eca1dde003b
takumitakeno <matchball.shine@gmail.com>
2020-04-07 10:51:28 +0000

    actioncable修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,8 +45,6 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  config.action_cable.allowed_request_origins = [ 'http://hoge.com/', /http:\/\/lets-continue.xyz.*/ ]
-
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
