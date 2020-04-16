290499464fe85aabcaa02ec71f8e79a1bc1e7de3
takumitakeno <matchball.shine@gmail.com>
2020-04-16 07:09:43 +0000

    goods_controller　修正

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,15 +1,15 @@
 class GoodsController < ApplicationController
 	before_action :authenticate_user!
 	def create
-		 @post = Post.find(params[:post_id])
-         good = current_user.goods.new(post_id: @post.id)
-         good.save
+    @post = Post.find(params[:post_id])
+			good = current_user.goods.new(post_id: @post.id)
+			good.save
 	end
 
 	def destroy
 		@post = Post.find(params[:post_id])
-        good = current_user.goods.find_by(post_id: @post.id)
-        good.destroy
+			good = current_user.goods.find_by(post_id: @post.id)
+			good.destroy
 	end
 end
 
