01b766f99aebc863c00c22bdf45eda788efa6725
takumitakeno <matchball.shine@gmail.com>
2020-04-19 06:52:37 +0000

    user.rbアソシエーションテスト追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user do
     user_name              {"test"}
+    # emailは重複すると登録できない
     sequence(:email) { |n| "test#{n}@example.com"}
     password              {"12345678"}
     password_confirmation {"12345678"}
