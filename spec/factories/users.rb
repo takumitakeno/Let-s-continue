06d9c704be63d3e768dfce215e0ed193f6c9178d
takumitakeno <matchball.shine@gmail.com>
2020-04-19 04:50:25 +0000

    testファイル追加

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {"test"}
+    email                 {"test@gmail.com"}
+    password              {"12345678"}
+    password_confirmation {"12345678"}
+  end
+end
