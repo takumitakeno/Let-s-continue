deba1afc06ef5312dccc169fb010527070d7b91c
takumitakeno <matchball.shine@gmail.com>
2020-05-01 02:51:16 +0000

    migration追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
 
+  devise_for :admins,
+    controllers: {
+      sessions: 'admins/sessions',
+      registrations: "admins/registrations"
+  }
   devise_for :users,
-   controllers: {
-    sessions: 'users/sessions',
-    registrations: "users/registrations",
-    omniauth_callbacks: 'users/omniauth_callbacks'
+    controllers: {
+      sessions: 'users/sessions',
+      registrations: "users/registrations",
+      omniauth_callbacks: 'users/omniauth_callbacks'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_074045) do
+ActiveRecord::Schema.define(version: 2020_05_01_024215) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.boolean "admin", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "chats", force: :cascade do |t|
     t.string "message", null: false
