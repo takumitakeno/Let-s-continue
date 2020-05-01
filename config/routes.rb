65cfe4e99677edbec7afea4a895b854cfd9ca314
takumitakeno <matchball.shine@gmail.com>
2020-05-01 06:24:38 +0000

    routeにscope追加

--- config/routes.rb
+++ config/routes.rb
@@ -12,33 +12,29 @@ Rails.application.routes.draw do
       omniauth_callbacks: 'users/omniauth_callbacks'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # scope :admins do
-  # 	resources :users
-  # end
-  root 'posts#index'
-  get "posts/new" => "posts#new"
-  get "homes/top" => "homes#top"
-  get "homes/about" => "homes#about"
-  get "homes/community" => "homes#community"
-  get "users/bmi" => "homes#bmi"
-  post "users/bmi_Calculation" => "homes#bmi_calculation"
-  get "posts/tags" => "posts#post_tags_index"
-  get "training_record/new" => "training_records#new"
-
-  resources :users do
-    get :follows, on: :member
-    get :followers, on: :member
-  end
-
-  resources :rooms, only: [:index, :show, :create]
-  resources :relation_ships, only: [:create, :destroy]
-  resources :training_records, only: [:index, :show, :create, :destroy]
-  resources :training_menus, only: [:index]
-
-  resources :posts do
-    resources :post_messages, only: [:create, :destroy]
-    resource :goods, only: [:create, :destroy]
+  root 'homes#top'
+  
+  scope :users do
+    get "posts/new" => "posts#new"
+    get "homes/top" => "homes#top"
+    get "homes/about" => "homes#about"
+    get "homes/community" => "homes#community"
+    get "users/bmi" => "homes#bmi"
+    post "users/bmi_Calculation" => "homes#bmi_calculation"
+    get "posts/tags" => "posts#post_tags_index"
+    get "training_record/new" => "training_records#new"
+    resources :users do
+      get :follows, on: :member
+      get :followers, on: :member
+    end
+    resources :rooms, only: [:index, :show, :create]
+    resources :relation_ships, only: [:create, :destroy]
+    resources :training_records, only: [:index, :show, :create, :destroy]
+    resources :training_menus, only: [:index]
+    resources :posts do
+      resources :post_messages, only: [:create, :destroy]
+      resource :goods, only: [:create, :destroy]
+    end
   end
 
   namespace :admins do
